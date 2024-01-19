b92bf6eb1872b79b0658c9c5a30cb5ba73375374
LauraSabrin <waila_05@hotmail.com>
2024-01-19 11:19:48 +0100

    deletedpartials

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
+//FORM:
+const form = document.querySelector('.js-form-search');
+const searchInput = document.querySelector('.js-input-search');
+const searchButton = document.querySelector('.js-search-btn');
+const resultList = document.querySelector('.js-result-list');
+
+const resetButton = document.querySelector('.js-reset-btn');
+let results = [];
+let favorites = [];
+
+const searchAnime =()=> {
+    fetch('https://api.jikan.moe/v4/anime/{id}/full'),
+    .then((response) => response.json())
+    .then ((data => )=> {
+    results = data.titles + data.images
+    });
+};
+
+
+// const handleSearch =()=> {}
+
+// 
+
+//click event listener on search button:
+searchButton.addEventListener('click', handleSearch);
+
+
 
--- src/js/variables.js
+++
@@ -1,8 +0,0 @@
-//ordenar en varias secciones para buscar más rápido
-//FORM:
-const form = document.querySelector('.js-form-search');
-const searchInput = document.querySelector('.js-input-search');
-const searchButton = document.querySelector('.js-search-btn');
-const resetButton = document.querySelector('.js-reset-btn');
-
-
