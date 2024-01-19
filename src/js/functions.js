27089b01e22aac58bc71b7e64d69de0a0d58ba9e
LauraSabrin <waila_05@hotmail.com>
2024-01-19 01:42:23 +0100

    first steps

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,1 +1,10 @@
 //ordenar en varios apartados al principo, luego borrar:
+
+
+const handleSearch =()=> {
+fetch('https://docs.api.jikan.moe/'),
+.then((response) => response.json())
+.then ((data => ))
+}
+
+searchButton.addEventListener('click', handleSearch);
